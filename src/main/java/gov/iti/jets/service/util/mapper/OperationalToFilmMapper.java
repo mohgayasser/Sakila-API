3f589462d06e42821065129b0d8c217483837bbc
mohgayasser <mohgayasserelsaid@gmail.com>
2023-11-27 20:59:38 +0200

    RevertSS OMPORTS

--- src/main/java/gov/iti/jets/service/util/mapper/OperationalToFilmMapper.java
+++ src/main/java/gov/iti/jets/service/util/mapper/OperationalToFilmMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.factory.Mappers;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 @Mapper
 public interface OperationalToFilmMapper {
 
